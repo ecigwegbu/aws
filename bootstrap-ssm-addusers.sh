d57c79e584bb9fd4084d39b7e251412dc063bd88
ecigwegbu <igwegbu@gmail.com>
2024-02-11 11:13:56 +0000

    Added script to bootstrap ssm and additional users

---
+++ bootstrap-ssm-addusers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script bootstraps AWS ssm function when creating EC2 instance based on a RHEL AMI
+# and creates additional users based on a list of user names
+# The users will connect to the EC2 instance from putty using the same
+# private key used by the default user ec2-user
+
+# (c) 2024 Unix Training Academy
+# Author: Elias Igwegbu
+
+# Install, Start and Enable Amazon SSM Agent (if using RHEL AMI)
+if [[ -n $(grep -i 'Red Hat Enterprise Linux' /etc/os-release) ]]; then
+  dnf install -y https://s3.us-east-1.amazonaws.com/amazon-ssm-us-east-1/latest/linux_amd64/amazon-ssm-agent.rpm
+   systemctl enable --now amazon-ssm-agent
+fi
+
+# create additional users and enable ssh authentication with the existing key-pair
+for user in elias richard donald; do  # do this for each of the three named users
+  if [[ -z $(getent passwd ${user}) ]]; then  # but only if they don't already exist
+    sudo useradd ${user}
+    sudo mkdir /home/${user}/.ssh
+    sudo cp -R /home/ec2-user/.ssh/authorized_keys\
+      /home/${user}/.ssh/authorized_keys
+    sudo chown -R ${user}:${user} /home/${user}/.ssh
+    sudo chmod 700 /home/${user}/.ssh
+    sudo chmod 600 /home/${user}/.ssh/authorized_keys
+  fi
+done
